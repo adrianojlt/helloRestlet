62a94cb5fd8685450f767e0db8671e98be589504
adriano <adrianojlt@gmail.com>
2014-10-02 23:34:01 +0100

    sql files edit

--- etc/create.sql
+++ etc/create.sql
@@ -9,7 +9,6 @@ INSERT INTO groups ( id , name ) VALUES ( 2 , 'manager');
 INSERT INTO groups ( id , name ) VALUES ( 3 , 'users');
 INSERT INTO groups ( id , name ) VALUES ( 4 , 'guests');
 
--- update groups set name='manager' where id = 2;
 
 create table users (
 	id int not null auto_increment,
@@ -20,8 +19,12 @@ create table users (
 );
 
 INSERT INTO users ( id , id_group , name , email ) VALUES ( 1 , 1 , 'adriano' , 'adriano@mail.com');
-INSERT INTO users ( id , id_group , name , email ) VALUES ( 1 , 1 , 'adriano' , 'adriano@mail.com');
-UPDATE users SET email = 'adriano@mail.com' WHERE id = 1;
+INSERT INTO users ( id , id_group , name , email ) VALUES ( 2 , 2 , 'manager1' , 'manager1@mail.com');
+INSERT INTO users ( id , id_group , name , email ) VALUES ( 3 , 2 , 'manager2' , 'manager2@mail.com');
+INSERT INTO users ( id , id_group , name , email ) VALUES ( 4 , 2 , 'manager3' , 'manager3@mail.com');
+INSERT INTO users ( id , id_group , name , email ) VALUES ( 5 , 3 , 'user1' , 'user1@mail.com');
+INSERT INTO users ( id , id_group , name , email ) VALUES ( 6 , 3 , 'user2' , 'user2@mail.com');
+INSERT INTO users ( id , id_group , name , email ) VALUES ( 7 , 4 , 'guest1' , 'guest1@mail.com');
 
 create table clients (
 	id int not null auto_increment,
@@ -30,6 +33,12 @@ create table clients (
 	email varchar(100) not null,
 	PRIMARY KEY(id)
 );
+
+INSERT INTO clients ( id , id_user , name , email ) VALUES ( 1 , 5 , 'client1' , 'client1@mail.com');
+INSERT INTO clients ( id , id_user , name , email ) VALUES ( 2 , 5 , 'client2' , 'client2@mail.com');
+INSERT INTO clients ( id , id_user , name , email ) VALUES ( 3 , 5 , 'client3' , 'client3@mail.com');
+INSERT INTO clients ( id , id_user , name , email ) VALUES ( 4 , 6 , 'client4' , 'client4@mail.com');
+INSERT INTO clients ( id , id_user , name , email ) VALUES ( 5 , 6 , 'client5' , 'client5@mail.com');
 
 
 
--- etc/query.sql
+++ etc/query.sql
@@ -1,12 +1,17 @@
 show databases;
-
-create database hellorestlet;
 use hellorestlet;
 show tables;
+
 select * from groups;
 select * from users;
 select * from clients;
+
 desc users;
+
+-- UPDATES
+update groups set name='manager' where id = 2;
+UPDATE users SET email = 'adriano@mail.com' WHERE id = 1;
+
 
 
 
